9f2dc9bd29d61f0efeffe91bf8d2b7831efc723b
ira <145407644+winterhyacinth@users.noreply.github.com>
2025-10-08 01:34:32 +0000

    added customized font

--- blog/settings.py
+++ blog/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = '/static/'
 STATIC_ROOT = "/home/winterhyacinth/blog/static"
 STATICFILES_DIRS = [BASE_DIR /"blog" / "static"]
 
