c2937436b596247a0181f70132714d6a043d4edb
ira <145407644+winterhyacinth@users.noreply.github.com>
2025-10-12 04:32:43 +0000

    even more debugging for python anywhere

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
+from django import forms
+from ckeditor.widgets import CKEditorWidget
 from .models import Recipe, Post
 
 # Register your models here.
 
-admin.site.register(Recipe)
-admin.site.register(Post)
+class PostAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorWidget())
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+admin.site.register(Post, PostAdmin)
